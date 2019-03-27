8fd65b91880477e93a187dc1b4ac1c2119e392cc
Sophie Gu <sophieqgu@gmail.com>
2019-03-27 17:22:54 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,12 +3,12 @@ require 'pry'
 class Artist
   attr_accessor :name
   attr_reader :songs
-  extend Memorable, Findable
-  include Paramable
+  extend Memorable::ClassMethods, Findable
+  include Memorable::InstanceMethods, Paramable
   @@artists = []
 
   def initialize
-    @@artists << self
+    super 
     @songs = []
   end
 
