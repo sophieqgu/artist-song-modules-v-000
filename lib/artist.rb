be6e9e34dd5f35d9f0396fc52ecc20cb337cd877
Sophie Gu <sophieqgu@gmail.com>
2019-03-27 16:59:15 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Artist
   attr_accessor :name
   attr_reader :songs
-
+  extend Memorable 
   @@artists = []
 
   def initialize
@@ -17,14 +17,6 @@ class Artist
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Song
   attr_accessor :name
   attr_reader :artist
-
+  extend Memorable
   @@songs = []
 
   def initialize
@@ -16,14 +16,6 @@ class Song
 
   def self.all
     @@songs
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def artist=(artist)
