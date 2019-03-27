2b88df5c96bfbba9f2dedc432cfbb35e2f1accfc
Sophie Gu <sophieqgu@gmail.com>
2019-03-27 17:22:27 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,9 +1,15 @@
 module Memorable 
-  def reset_all
-    self.all.clear 
-  end 
+  module ClassMethods
+    def reset_all
+      self.all.clear 
+    end 
+    
+    def count
+      self.all.count
+    end 
   
-  def count
-    self.all.count
-  end 
+  module InstanceMethods 
+    def initialize 
+      self.class.all << self 
+    end 
 end 
